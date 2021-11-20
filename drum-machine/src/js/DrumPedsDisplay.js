5af90e87a7047713ea45ae0a5aae83a2c74c0b02
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-20 21:06:19 +0200

    # Generate li item for every dataObj in the "bankOne" array

--- drum-machine/src/js/DrumPedsDisplay.js
+++ drum-machine/src/js/DrumPedsDisplay.js
@@ -3,6 +3,17 @@ import { bankOne, bankTwo } from '../data/drumPedsData';
 
 export class DrumPedsDisplay extends Component {
   render() {
-    return <div id='display'></div>;
+    return (
+      <div id='display'>
+        <ul className='peds-list'>
+          {/* TODO REPLACE LI WITH COMPONENT */}
+          {bankOne.map((pedData) => (
+            <li key={pedData.id} id={pedData.id} className='drum-pad'>
+              {pedData.keyTrigger}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
