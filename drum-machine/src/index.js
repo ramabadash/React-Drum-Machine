4b6c9f484d91b6898162c3c5496f628bcaf0abfe
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-20 20:48:35 +0200

    # Create Drum-machine component

--- drum-machine/src/index.js
+++ drum-machine/src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
+import { DrumMachine } from './js/DrumMachine';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DrumMachine />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ drum-machine/src/js/DrumMachine.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export class DrumMachine extends Component {
+  render() {
+    return (
+      <div id='drum-machine'>
+        <div id='display'>Drum Machine</div> {/* TODO REPLACE WITH COMPONENT*/}
+      </div>
+    );
+  }
+}
