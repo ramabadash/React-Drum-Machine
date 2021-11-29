6cdf559ed49cfe122688ecc9d4b425d5c012c73a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-29 15:55:49 +0200

    # On "drum-pad" click triggred the child audio

--- drum-machine/src/js/DrumPed.js
+++ drum-machine/src/js/DrumPed.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/DrumPed.css';
 
 function DrumPed({ id, audio, keyTrigger }) {
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    audioRef.current.play();
+  };
   return (
-    <li className='drum-pad' id={id}>
-      <audio src={audio} className='clip' id={keyTrigger} controls />
+    <li className='drum-pad' id={id} onClick={playSound}>
+      <audio ref={audioRef} src={audio} className='clip' id={keyTrigger} controls />
       {keyTrigger}
     </li>
   );
